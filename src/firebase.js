7d02112870c34e9a6b5993412d012fa96529265c
Shree Charan ML <shreecharan150718@gmail.com>
2021-06-01 22:30:23 +0530

    Update firebase.js
    removed firebase creds

--- src/firebase.js
+++ src/firebase.js
@@ -5,14 +5,8 @@ import "firebase/auth";
 import "firebase/firestore";
 import "firebase/performance";
 import "firebase/storage";
-const firebaseConfig = {
-  apiKey: "AIzaSyCDRMXUhPXi9XVQ0vrv3iXeHzhBsqzqgtU",
-  authDomain: "bakery-a9c04.firebaseapp.com",
-  projectId: "bakery-a9c04",
-  storageBucket: "bakery-a9c04.appspot.com",
-  messagingSenderId: "175876821083",
-  appId: "1:175876821083:web:b810884c7d4ed05dcc2f7e",
-  measurementId: "G-156196JB3C"
-};
+
+//replace this with your firebase config
+const firebaseConfig = {};
 firebase.initializeApp(firebaseConfig);
 export default firebase;  
